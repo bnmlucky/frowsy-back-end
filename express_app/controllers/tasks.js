e59fe10e0fc641fb6ad8eb1bd5b61c6cefcf7bdf
Alice D'Arcangelo <alicedarcangelo@Alices-MacBook-Pro.local>
2019-11-24 12:37:03 -0500

    edit route working

--- express_app/controllers/tasks.js
+++ express_app/controllers/tasks.js
@@ -79,47 +79,55 @@ router.delete("/:userid/:taskid", (req, res) => {
   });
 });
 /********   UPDATE    ************/
-// router.put("/:id", (req, res) => {
-//   const tasksArray = req.session.currentUser.tasks.map(task => {
-//     if (task._id == req.params.id) {
-//       return {
-//         _id: req.params.id,
-//         description: req.body.description,
-//         progress: req.body.assigned,
-//         __v: 0
-//       };
-//     } else {
-//       return task;
-//     }
-//   });
-//   User.update(
-//     {
-//       _id: req.body.user
-//     },
-//     {
-//       $set: { tasks: tasksArray }
-//     },
-//     { new: true },
-//     (error, updatedUser) => {
-//       if (error) {
-//         console.log(error);
-//       } else {
-//         console.log(updatedUser);
-//       }
-//     }
-//   );
-//   Tasks.findByIdAndUpdate(
-//     req.params.id,
-//     req.body,
-//     { new: true },
-//     (err, updatedTask) => {
-//       if (err) {
-//         res.status(400).json({ error: err.message });
-//       }
-//       res.status(200).json(updatedTask);
-//     }
-//   );
-// });
+router.put("/:userid/:taskid", (req, res) => {
+  console.log(req.body);
+  const taskFunctions = tasks => {
+    const tasksArray = tasks.map(task => {
+      if (task._id == req.params.taskid) {
+        return {
+          _id: req.params.taskid,
+          description: req.body.description,
+          progress: req.body.assigned,
+          __v: 0
+        };
+      } else {
+        return task;
+      }
+    });
+    User.update(
+      { _id: req.params.userid },
+      {
+        $set: { tasks: tasksArray }
+      },
+      { new: true },
+      (error, updatedUser) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(updatedUser);
+        }
+      }
+    );
+  };
+  User.findById(req.params.userid, (error, foundUser) => {
+    if (error) {
+      console.log(error);
+    } else {
+      taskFunctions(foundUser.tasks);
+    }
+  });
+  Tasks.findByIdAndUpdate(
+    req.params.taskid,
+    req.body,
+    { new: true },
+    (err, updatedTask) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      }
+      res.status(200).json(updatedTask);
+    }
+  );
+});
 //EXPORTS
 module.exports = router;
 
